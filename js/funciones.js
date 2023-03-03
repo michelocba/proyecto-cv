9ba3995d270f301b9892e5ae59e2dff97692de80
michelocba <hectorcba@gmail.com>
2023-03-03 13:59:51 -0300

    Comportamientos Redes Sociales

--- js/funciones.js
+++ js/funciones.js
@@ -24,3 +24,23 @@ document.getElementById('menu-nombre').onclick = function(){
 document.getElementById('boton-enviar').onclick = function(){
   ocultar_mostar_datos_iniciales('formulario-contacto');
 }
+
+document.getElementById('facebook').onclick = function(){
+  window.open('https://www.facebook.com/MICHELO.HDM');
+}
+
+document.getElementById('instagram').onclick = function(){
+  window.open('https://www.instagram.com/michelocba/');
+}
+
+document.getElementById('twitter').onclick = function(){
+  window.open('https://twitter.com/hectormichelet1');
+}
+
+document.getElementById('linkedin').onclick = function(){
+  window.open('https://www.linkedin.com/in/h%C3%A9ctor-alberto-micheletti-1308471b/');
+}
+
+document.getElementById('whatsapp').onclick = function(){
+  window.open('https://api.whatsapp.com/send?phone=542548356842');
+}
